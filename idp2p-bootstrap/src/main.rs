9f910bace5fb7e1e4b300d07e567721da3960bc3
ademcaglin <adem.caglin@gmail.com>
2022-03-12 15:24:17 +0300

    Relay implemented

--- idp2p-bootstrap/src/main.rs
+++ idp2p-bootstrap/src/main.rs
@@ -1,13 +1,8 @@
 use crate::ping::PingEvent;
 use idp2p_common::decode_sized;
-use libp2p::autonat;
 use libp2p::core::identity;
 use libp2p::core::PeerId;
 use libp2p::futures::StreamExt;
-use libp2p::gossipsub::{
-    Gossipsub, GossipsubConfigBuilder, GossipsubEvent, GossipsubMessage, MessageAuthenticity,
-    MessageId, ValidationMode,
-};
 use libp2p::identify::{Identify, IdentifyConfig, IdentifyEvent};
 use libp2p::ping;
 use libp2p::ping::Ping;
@@ -15,9 +10,15 @@ use libp2p::swarm::NetworkBehaviourEventProcess;
 use libp2p::swarm::{Swarm, SwarmEvent};
 use libp2p::NetworkBehaviour;
 use libp2p::{development_transport, rendezvous};
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::time::Duration;
+use libp2p::relay::v2::relay::{self, Relay};
+//use std::collections::hash_map::DefaultHasher;
+//use std::hash::{Hash, Hasher};
+//use std::time::Duration;
+//use libp2p::autonat;
+/*use libp2p::gossipsub::{
+    Gossipsub, GossipsubConfigBuilder, GossipsubEvent, GossipsubMessage, MessageAuthenticity,
+    MessageId, ValidationMode,
+};*/
 
 #[tokio::main]
 async fn main() {
@@ -25,7 +26,7 @@ async fn main() {
     let bytes: [u8; 32] = decode_sized(&args[1]).unwrap();
     let key = identity::ed25519::SecretKey::from_bytes(bytes).unwrap();
     let identity = identity::Keypair::Ed25519(key.into());
-    let message_id_fn = |message: &GossipsubMessage| {
+    /*let message_id_fn = |message: &GossipsubMessage| {
         let mut s = DefaultHasher::new();
         message.data.hash(&mut s);
         MessageId::from(s.finish().to_string())
@@ -40,13 +41,14 @@ async fn main() {
     let auto_nat =
         autonat::Behaviour::new(identity.public().to_peer_id(), autonat::Config::default());
     let gossipsub_result = Gossipsub::new(MessageAuthenticity::Anonymous, gossipsub_config);
-    let gossipsub = gossipsub_result.expect("Correct configuration");
+    let gossipsub = gossipsub_result.expect("Correct configuration");*/
     let ping = Ping::new(ping::Config::new().with_keep_alive(true));
     let rendezvous = rendezvous::server::Behaviour::new(rendezvous::server::Config::default());
     let identify = Identify::new(IdentifyConfig::new(
         "rendezvous-example/1.0.0".to_string(),
         identity.public(),
     ));
+    let relay = Relay::new(identity.public().to_peer_id(), Default::default());
     println!("Peer id: {}", identity.public().to_peer_id());
     let mut swarm = Swarm::new(
         development_transport(identity.clone()).await.unwrap(),
@@ -54,8 +56,9 @@ async fn main() {
             identify,
             rendezvous,
             ping,
-            auto_nat,
-            gossipsub,
+            relay
+            //auto_nat,
+            //gossipsub,
         },
         PeerId::from(identity.public()),
     );
@@ -82,7 +85,8 @@ pub enum BootstrapEvent {
     Rendezvous(rendezvous::server::Event),
     Ping(PingEvent),
     Identify(IdentifyEvent),
-    AutoNat(autonat::Event),
+    Relay(relay::Event)
+    //AutoNat(autonat::Event),
 }
 
 #[derive(NetworkBehaviour)]
@@ -92,8 +96,9 @@ struct BootstrapBehaviour {
     identify: Identify,
     rendezvous: rendezvous::server::Behaviour,
     ping: Ping,
-    auto_nat: autonat::Behaviour,
-    gossipsub: Gossipsub,
+    relay: Relay
+    //auto_nat: autonat::Behaviour,
+    //gossipsub: Gossipsub,
 }
 
 impl NetworkBehaviourEventProcess<IdentifyEvent> for BootstrapBehaviour {
@@ -102,9 +107,9 @@ impl NetworkBehaviourEventProcess<IdentifyEvent> for BootstrapBehaviour {
     }
 }
 
-impl NetworkBehaviourEventProcess<autonat::Event> for BootstrapBehaviour {
-    fn inject_event(&mut self, event: autonat::Event) {
-        //println!("{:?}", event);
+impl NetworkBehaviourEventProcess<relay::Event> for BootstrapBehaviour {
+    fn inject_event(&mut self, event: relay::Event) {
+        println!("{:?}", event);
     }
 }
 
@@ -120,8 +125,15 @@ impl NetworkBehaviourEventProcess<rendezvous::server::Event> for BootstrapBehavi
     }
 }
 
-impl NetworkBehaviourEventProcess<GossipsubEvent> for BootstrapBehaviour {
+/*impl NetworkBehaviourEventProcess<GossipsubEvent> for BootstrapBehaviour {
     fn inject_event(&mut self, e: GossipsubEvent) {
         println!("{:?}", e);
     }
 }
+
+
+impl NetworkBehaviourEventProcess<autonat::Event> for BootstrapBehaviour {
+    fn inject_event(&mut self, event: autonat::Event) {
+        //println!("{:?}", event);
+    }
+}*/
