221a49387586c91f17ac684c481369069ef50e96
ademcaglin <adem.caglin@gmail.com>
2022-03-19 19:13:29 +0300

    Remove stdin

--- idp2p-relay/src/behavior.rs
+++ idp2p-relay/src/behavior.rs
@@ -2,14 +2,12 @@ use idp2p_node::node::build_gossipsub;
 use idp2p_node::node::build_transport;
 use libp2p::identity::Keypair;
 use libp2p::swarm::SwarmBuilder;
-use libp2p::Multiaddr;
 use libp2p::PeerId;
 use libp2p::{
     gossipsub::{Gossipsub, GossipsubEvent, IdentTopic},
     swarm::Swarm,
     NetworkBehaviour,
 };
-use std::str::FromStr;
 
 pub async fn build_swarm(local_key: Keypair) -> Swarm<IdentityRelayBehaviour> {
     let transport = build_transport(local_key.clone()).await;
@@ -42,25 +40,6 @@ impl From<GossipsubEvent> for IdentityRelayEvent {
     fn from(event: GossipsubEvent) -> Self {
         IdentityRelayEvent::Gossipsub(event)
     }
-}
-
-pub fn run_command(
-    input: &str,
-    swarm: &mut Swarm<IdentityRelayBehaviour>,
-) -> idp2p_common::anyhow::Result<()> {
-    let split = input.split(" ");
-    let input: Vec<&str> = split.collect();
-    match input[0] {
-        "connect" => {
-            let to_dial = format!("/ip4/{}/tcp/{}", input[1], input[2]);
-            let addr: Multiaddr = to_dial.parse().unwrap();
-            let peer_id = PeerId::from_str(input[3])?;
-            swarm.dial(addr)?;
-            swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
-        }
-        _ => {}
-    }
-    Ok(())
 }
 
 impl IdentityRelayBehaviour {
--- idp2p-relay/src/main.rs
+++ idp2p-relay/src/main.rs
@@ -1,31 +1,41 @@
-use self::behavior::build_swarm;
-use self::behavior::run_command;
-use self::behavior::IdentityRelayEvent;
-use dotenv::dotenv;
+use libp2p::futures::StreamExt;
+use std::str::FromStr;
+use libp2p::PeerId;
+use libp2p::Multiaddr;
+use self::behavior::{build_swarm, IdentityRelayEvent};
 use idp2p_common::anyhow::Result;
 use idp2p_common::ed_secret::EdSecret;
-use libp2p::futures::StreamExt;
 use libp2p::identity::ed25519::SecretKey;
 use libp2p::identity::Keypair;
 use libp2p::swarm::SwarmEvent;
-use tokio::io::AsyncBufReadExt;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "idp2p", about = "Usage of idp2p.")]
+struct Opt {
+    #[structopt(short = "c", long = "connect")]
+    connect: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+    let opt = Opt::from_args();
     let secret = EdSecret::new();
     let secret_key = SecretKey::from_bytes(secret.to_bytes())?;
     let local_key = Keypair::Ed25519(secret_key.into());
     let mut swarm = build_swarm(local_key.clone()).await;
     swarm.listen_on("/ip4/0.0.0.0/tcp/43727".parse()?)?;
-
+    if let Some(connect) = opt.connect{
+        let split: Vec<&str> = connect.split("/").collect();
+        let to_dial = format!("/ip4/{}/tcp/{}", split[2], split[4]);
+        let addr: Multiaddr = to_dial.parse().unwrap();
+        let peer_id = PeerId::from_str(split[6])?;
+        swarm.dial(addr)?;
+        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+    }
+   
     loop {
         tokio::select! {
-            line = stdin.next_line() => {
-                let line = line?.expect("stdin closed");
-                run_command(&line, &mut swarm)?;
-            }
             swarm_event = swarm.select_next_some() => {
                 match swarm_event {
                     SwarmEvent::NewListenAddr { address, .. } => {
