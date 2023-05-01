4822a21508ae135e1dd10c4f8920bd6651e34865
Adem Çağlın <adem.caglin@gmail.com>
2023-05-01 21:45:34 +0300

    Notary impl

--- core/identity/build.rs
+++ core/identity/build.rs
@@ -1,7 +1,5 @@
 fn main() {
     let mut prost_build = prost_build::Config::new();
-    prost_build.protoc_arg("--experimental_allow_proto3_optional");
-
     prost_build
         .compile_protos(&["proto/id.proto"], &["proto"])
         .unwrap();
